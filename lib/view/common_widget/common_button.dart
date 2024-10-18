7eb603491b5ef9cbb7ccf8775b9c6ed69d120d3e
Shovon0017 <Shimantomostofa@gmail.com>
2024-10-18 14:52:32 +0600

    Homemade Haven extra

--- lib/view/common_widget/common_button.dart
+++ lib/view/common_widget/common_button.dart
@@ -22,7 +22,7 @@ class CommonButton extends StatelessWidget {
     return InkWell(
       onTap: onTap,
       child: Card(
-        color: buttonColor ??const Color(0xff9a0000),
+        color: buttonColor ??Color(0xffa1447d),
         child: SizedBox(
           height: buttonHeight ?? 50,
           width: buttonWidth ?? MediaQuery.sizeOf(context).width,
