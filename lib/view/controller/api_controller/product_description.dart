5928c9854871fd693dd35f09b80dea0f73a111b0
Shovon0017 <Shimantomostofa@gmail.com>
2024-10-18 14:40:34 +0600

    Homemade Haven extra

--- lib/view/controller/api_controller/product_description.dart
+++ lib/view/controller/api_controller/product_description.dart
@@ -1,13 +1,13 @@
-import 'dart:convert';
-import 'package:flutter/material.dart';
-class ProductInfoService {
-  static Future<ProductInfoModel?> productInfoService({required String id}) async {
-    try {
-      ProductInfoModel descriptionModel = ProductInfoModel.fromJson(jsonDecode(jsonEncode(ProductInfoData.productInfoData)));
-      return descriptionModel;
-    } catch (e) {
-      debugPrint("Error : $e");
-    }
-    return null;
-  }
-}
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// class ProductInfoService {
+//   static Future<ProductInfoModel?> productInfoService({required String id}) async {
+//     try {
+//       ProductInfoModel descriptionModel = ProductInfoModel.fromJson(jsonDecode(jsonEncode(ProductInfoData.productInfoData)));
+//       return descriptionModel;
+//     } catch (e) {
+//       debugPrint("Error : $e");
+//     }
+//     return null;
+//   }
+// }
