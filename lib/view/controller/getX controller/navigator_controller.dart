cea9472460945f2afd0c435404020c19b450c7e0
Shovon0017 <Shimantomostofa@gmail.com>
2024-10-28 21:56:21 +0600

    Homemade Haven home page correction

--- lib/view/controller/getX controller/navigator_controller.dart
+++ lib/view/controller/getX controller/navigator_controller.dart
@@ -1,6 +1,7 @@
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:homemade_haven/view/screen/Profile/profile.dart';
 import 'package:homemade_haven/view/screen/home/home.dart';
 class BottomNavBarController extends GetxController {
   final pageController = PageController(initialPage: 0);
@@ -13,7 +14,7 @@ class BottomNavBarController extends GetxController {
   List<Widget> bottomBarPages = [
     const Home(),
     // const Cart(),
-    // const Profile()
+    const Profile()
   ];
   @override
   void onInit() {
---
+++ lib/view/screen/Profile/profile.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:homemade_haven/view/common_widget/CommonIcon.dart';
+import 'package:homemade_haven/view/common_widget/common_button.dart';
+import 'package:homemade_haven/view/controller/getX%20controller/logout.dart';
+import 'package:homemade_haven/view/screen/notification/notification.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    LogOutService controller = Get.put(LogOutService());
+    return Scaffold(
+      backgroundColor: const Color(0xffFFFFFF),
+      appBar: AppBar(
+        backgroundColor: const Color(0xffFFFFFF),
+        title: SizedBox(
+            height: 32, width: 114, child: Image.asset("images/pcmart.jpg")),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: CommonIconButton(onTap: () {
+              Get.to(() => const NotificationShow());
+            }),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text(
+                "Profile",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+              SizedBox(
+                height: 120,
+                width: double.infinity,
+                child: Card(
+                  elevation: 5,
+                  color: const Color(0xff9a0000),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(
+                          height: 100,
+                          width: 100,
+                          child:
+                              Card(child: Image.asset("images/Shovonpic.jpg"))),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Name:Md Shimanto Mostofa",
+                            style: TextStyle(
+                                overflow: TextOverflow.ellipsis,
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          RichText(
+                              text: const TextSpan(children: [
+                            TextSpan(
+                                text: "Email:",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            TextSpan(
+                                text: "Shovon@gmail.com",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 15))
+                          ])),
+                          RichText(
+                              text: const TextSpan(children: [
+                            TextSpan(
+                                text: "Phone:",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            TextSpan(
+                                text: "01643770017",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 15))
+                          ])),
+                          RichText(
+                              text: const TextSpan(children: [
+                            TextSpan(
+                                text: "Address:",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            TextSpan(
+                                text: "Bheramara,Kushtia",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 15))
+                          ]))
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CommonButton(
+                  buttonName: "Setting",
+                  onTap: () {
+
+                  }),
+              CommonButton(
+                  buttonName: "Support",
+                  onTap: () {
+
+                  }),
+              CommonButton(
+                  buttonName: "FAQ",
+                  onTap: () {
+
+                  }),
+              CommonButton(
+                  buttonName: "LogOut",
+                  onTap: () {
+                    controller.logOutFun();
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
