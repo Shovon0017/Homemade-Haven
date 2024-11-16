72aa846f0f7327ec78d3830afb43d8facac7c142
Shovon0017 <Shimantomostofa@gmail.com>
2024-11-16 19:53:26 +0600

    Homemade Haven home page correction

--- lib/view/screen/SliderAllProduct/SliderProduct.dart
+++ lib/view/screen/SliderAllProduct/SliderProduct.dart
@@ -54,7 +54,7 @@ class SliderProductScreen extends StatelessWidget {
                 ? const Center(child: CircularProgressIndicator())
                 : controller.recipeList.isEmpty
                 ? const Center(
-                child: CommonText(title: "Empty Prod List"))
+                child: CommonText(title: "Empty Pr List"))
                 : Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
