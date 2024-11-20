92e0d688bee773a3bbcb6afafbad375f37e24291
Shovon0017 <shimantomostofa@gmail.com>
2024-11-20 22:44:17 +0600

    Homemade Haven home page correction

--- lib/view/screen/notification/notification.dart
+++ lib/view/screen/notification/notification.dart
@@ -32,7 +32,7 @@ class NotificationShow extends StatelessWidget {
                     color: Colors.white,
                     child: Center(child: Padding(
                       padding: EdgeInsets.all(8.0),
-                      child: Text(" lasania turkeish dish is uploaded to our new recipes.",style: TextStyle(fontSize:15,fontWeight: FontWeight.bold),),
+                      child: Text(" lasani turkeish dish is uploaded to our new recipes.",style: TextStyle(fontSize:15,fontWeight: FontWeight.bold),),
                     )),
                   ),
                 );
